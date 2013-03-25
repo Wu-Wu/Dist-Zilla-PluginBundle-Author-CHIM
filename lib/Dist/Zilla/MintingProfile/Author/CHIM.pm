33e2d8d2709d2d63c6e816c5e21d5518459f6cc5
Anton Gerasimov <me@zyxmasta.com>
2013-03-25 07:25:55 +0000

    added minting profile

---
+++ lib/Dist/Zilla/MintingProfile/Author/CHIM.pm
@@ -0,0 +1,37 @@
+package Dist::Zilla::MintingProfile::Author::CHIM;
+
+# ABSTRACT: Make new modules like CHIM does
+
+use strict;
+use warnings;
+
+# VERSION
+# AUTHORITY
+
+use Moose;
+with 'Dist::Zilla::Role::MintingProfile::ShareDir';
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1; # End of Dist::Zilla::MintingProfile::Author::CHIM
+
+__END__
+
+=pod
+
+=head1 SYNOPSYS
+
+    % dzil new -P Author::CHIM Pretty::Cool::Module
+
+=head2 DESCRIPTION
+
+This installs a Dist::Zilla minting profile used by CHIM.
+
+=head2 SEE ALSO
+
+L<Dist::Zilla>
+
+L<Dist::Zilla::PluginBundle::Author::CHIM>
+
+=cut
